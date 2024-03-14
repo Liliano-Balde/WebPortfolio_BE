d89509f01442a0fab2e48bacb910a2b97142389b
Liliano-Balde <liliano.balde@hotmail.com>
2024-03-14 10:45:04 +0000

    intitial commit

---
+++ src/main/java/com/lbg/porfolio/WebPortfolioBeApplication.java
@@ -0,0 +1,13 @@
+package com.lbg.porfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPortfolioBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPortfolioBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lbg/porfolio/WebPortfolioBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbg.porfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPortfolioBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
